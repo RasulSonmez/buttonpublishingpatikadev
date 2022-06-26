c764b978a7b685f05bf401ff150fce5e7e814586
rasul sönmez <barbaros743@outlook.com>
2022-06-26 22:42:17 +0300

    first

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-import { ExampleComponent } from 'buttonpublishingpatikadev'
+import { ExampleComponent, Button } from 'buttonpublishingpatikadev'
 import 'buttonpublishingpatikadev/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <Button text='Click' type='primary' />
+      <Button text='Click' type='dashed' />
+      <Button text='Click' type='default' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Button = (props) => {
+  return (
+    <div>
+      <button className={`btntyp-btn ${props.type}`}>{props.text}</button>
+    </div>
+  )
+}
